d25c5f2e93f5b4d9883abb4995b08823d7cfb1f5
KlaasDeNys <klaas.denys@outlook.com>
2015-10-20 09:57:59 +0200

    s

--- code/LookupServer/INameServer.java
+++ code/LookupServer/INameServer.java
@@ -17,4 +17,6 @@ public interface INameServer extends Remote{
 
 	String lookUp (int id) throws RemoteException;	// Give the ip of the specified node.
 	boolean add (int id, String adr) throws RemoteException;	// Add a node by the given addres.
+	String getPrev(int id);
+	String getNext(int id);
 }
