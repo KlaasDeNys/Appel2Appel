6a521ff065d6bea962e5ef9d13aceb1464759c73
JensStoel <jens.donckers@student.uantwerpen.be>
2015-12-15 10:26:31 +0100

    IAgent verwijdert
    Ik heb even de IAgent verwijdert

--- Code/Alles/agent/Agent.java
+++ Code/Alles/agent/Agent.java
@@ -2,7 +2,7 @@ package agent;
 
 import java.io.Serializable;
 
-public abstract class Agent implements Runnable,  Serializable, IAgent{
+public abstract class Agent implements Runnable,  Serializable {
 
 	private static final long serialVersionUID = 1L;
 
